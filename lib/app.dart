0c173cbe49ed4f0007e9dae106ce81dfe6a9a8dd
Manish Adhikari <manishad@gmeremit.com>
2026-06-04 16:00:38 +0900

    feat(theme): add themeModeProvider and drive MaterialApp themeMode

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'providers/theme_mode_provider.dart';
 import 'router/app_router.dart';
 import 'theme/app_theme.dart';
 
-class DaysoffApp extends StatelessWidget {
+class DaysoffApp extends ConsumerWidget {
   const DaysoffApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
     return MaterialApp.router(
       title: 'daysoff',
       debugShowCheckedModeBanner: false,
       theme: DaysoffTheme.light(),
       darkTheme: DaysoffTheme.dark(),
-      themeMode: ThemeMode.light,
+      themeMode: themeMode,
       routerConfig: appRouter,
     );
   }
