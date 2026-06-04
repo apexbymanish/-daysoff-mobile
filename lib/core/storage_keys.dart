538ffee5ecfe0a7dcfb5da206566f8043d5cb869
Manish Adhikari <manishad@gmeremit.com>
2026-06-04 17:03:30 +0900

    chore(persistence): add get_storage, init in main, storage keys

---
+++ lib/core/storage_keys.dart
@@ -0,0 +1,8 @@
+/// Keys for the on-device key-value store (get_storage).
+class StorageKeys {
+  StorageKeys._();
+  static const country = 'selected_country';
+  static const year = 'selected_year';
+  static const themeMode = 'theme_mode';
+  static const onboardingSeen = 'onboarding_seen';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   runApp(const ProviderScope(child: DaysoffApp()));
 }
